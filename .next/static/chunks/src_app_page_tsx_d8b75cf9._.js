ce0f00ce86514197db097941ce08960155d0cbb6
ABRAR805 <143305941+ABRAR805@users.noreply.github.com>
2025-09-06 00:32:18 +0530

    Add .next/static/chunks/src_app_page_tsx_d8b75cf9._.js 1757098935487

---
+++ .next/static/chunks/src_app_page_tsx_d8b75cf9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_49112785._.js",
+  "static/chunks/node_modules_5d27ab35._.js"
+],
+    source: "dynamic"
+});
